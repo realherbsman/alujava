848de21dddc2fc708a219913df0957bb87be1d23
yakim <yakim@alujava>
2018-06-06 06:40:45 +0300

    RestControllerAdvice added

--- alujava.boot/alujava.boot.server/src/main/java/alujava/boot/server/controllers/ExceptionController.java
+++ alujava.boot/alujava.boot.server/src/main/java/alujava/boot/server/controllers/ExceptionController.java
@@ -4,25 +4,26 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
 
 import alujava.boot.server.exceptions.DuplicateCardException;
 import alujava.boot.server.exceptions.InvalidCardException;
 import alujava.boot.server.exceptions.NoCardException;
 
-@Controller
+@RestControllerAdvice(assignableTypes={DealerController.class})
 public class ExceptionController {
 
-	@ExceptionHandler
+	@ExceptionHandler(NoCardException.class)
 	public ResponseEntity<String> handleNoCard(NoCardException exception) {
 		return new ResponseEntity<String>("No card!", HttpStatus.NO_CONTENT);
 	}
 	
-	@ExceptionHandler
+	@ExceptionHandler(InvalidCardException.class)
 	public ResponseEntity<String> handleInvalidCard(InvalidCardException exception) {
 		return new ResponseEntity<String>("Invalid card!", HttpStatus.NO_CONTENT);
 	}
 	
-	@ExceptionHandler
+	@ExceptionHandler(DuplicateCardException.class)
 	public ResponseEntity<String> handleDuplicateCard(DuplicateCardException exception) {
 		return new ResponseEntity<String>("Duplicate card!", HttpStatus.NO_CONTENT);
 	}
